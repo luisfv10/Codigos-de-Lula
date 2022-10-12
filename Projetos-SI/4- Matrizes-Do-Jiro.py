953aa094795c2c09982e7d873c0d4e2aed24b6b3
Luis Filipe Vasconcelos Pascoal <luisfilipevasconcelospascoal@gmail.com>
2022-10-12 09:29:22 -0300

    Update and rename 4- MatrizesDoJiro.py to 4- Matrizes-Do-Jiro.py

--- Projetos-SI/4- Matrizes-Do-Jiro.py
+++ Projetos-SI/4- Matrizes-Do-Jiro.py
@@ -3,25 +3,25 @@
 # final do while rodada += 1 para finalizar o while
 # 2 while pra formar as duas matrizes
 # while rodada < N: numeros = input.split(' ') e depois matriz1.append(numeros):
-# CRIANDO AS MATRIZES
+#CRIANDO AS MATRIZES
 
 m1 = []
 m2 = []
 N = int(input())
 rodada = 0
 while rodada < N:
-    numeros_matrizes1 = input().split(' ')
-    for i in range(len(numeros_matrizes1)):
-        numeros_matrizes1[i] = int(numeros_matrizes1[i])
-    m1.append(numeros_matrizes1)
-    rodada += 1
+  numeros_matrizes1 = input().split(' ')
+  for i in range(len(numeros_matrizes1)):
+      numeros_matrizes1[i] = int(numeros_matrizes1[i])
+  m1.append(numeros_matrizes1)
+  rodada += 1
 rodada = 0
 while rodada < N:
-    numeros_matrizes2 = input().split(' ')
-    for i in range(len(numeros_matrizes2)):
-        numeros_matrizes2[i] = int(numeros_matrizes2[i])
-    m2.append(numeros_matrizes2)
-    rodada += 1
+  numeros_matrizes2 = input().split(' ')
+  for i in range(len(numeros_matrizes2)):
+      numeros_matrizes2[i] = int(numeros_matrizes2[i])
+  m2.append(numeros_matrizes2)
+  rodada += 1
 # m1 = [['31', '21', '46'], ['37', '9', '8'], ['37', '29', '2']]
 # m2 = [['10', '12', '40'], ['8', '11', '8'], ['26', '22', '11']]
 
@@ -32,14 +32,12 @@ qtd_op = int(input())
 operacoes = []
 rodada = 0
 while rodada < qtd_op:
-    op = input().split(' ')
+    op =  input().split(' ')
     operacoes.append(op)
     rodada += 1
 # OPERACOES CONCLUIDO
 # CRIAR FUNCOES CALCULOS PARA AS MATRIZES
 # SOMA
-
-
 def SomaMatrizes(M1, M2):
     resultado = []
     for a in range(len(M1)):
@@ -49,8 +47,6 @@ def SomaMatrizes(M1, M2):
     return resultado
 
 # SUBTRAÇAO
-
-
 def SubMatrizes(M1, M2):
     resultado = []
     for a in range(len(M1)):
@@ -60,15 +56,13 @@ def SubMatrizes(M1, M2):
     return resultado
 
 # MULTIPLICAÇAO
-
-
 def MultiMatrizes(M1, M2):
     def linha(matriz, n):
         return [i for i in matriz[n]]
 
     def coluna(matriz, n):
         return [i[n] for i in matriz]
-
+    
     m1linha = len(M1)
     m2coluna = len(M2[0])
 
@@ -80,14 +74,11 @@ def MultiMatrizes(M1, M2):
             resolucao[a].append(sum(multi))
     return resolucao
 
-
 def EscalarMatrizes(matriz, k):
     resultado = []
     for l in matriz:
         resultado.append([elemento * k for elemento in l])
     return resultado
-
-
 # FUNCOES CALCULOS CONCLUIDOS
 # CALCULO
 # m1 primeiro
@@ -132,25 +123,25 @@ for operacao in operacoes:
         m1 = resultado
 # m2 primeiro
     elif operacao[0] == 'm2' and '*' not in operacao:
-        Ma = None
-        if operacao[2] == 'm1':
-            Ma = m1
-        else:
-            Ma = m2
-        Mb = None
-        if operacao[4] == 'm1':
-            Mb = m1
-        else:
-            Mb = m2
-        if operacao[3] == '+':
-            resultado = SomaMatrizes(Ma, Mb)
-            m2 = resultado
-        elif operacao[3] == '-':
-            resultado = SubMatrizes(Ma, Mb)
-            m2 = resultado
-        elif operacao[3] == '.':
-            resultado = MultiMatrizes(Ma, Mb)
-            m2 = resultado
+            Ma = None
+            if operacao[2] == 'm1':
+                Ma = m1
+            else:
+                Ma = m2
+            Mb = None
+            if operacao[4] == 'm1':
+                Mb = m1
+            else:
+                Mb = m2
+            if operacao[3] == '+':
+                resultado = SomaMatrizes(Ma, Mb)
+                m2 = resultado
+            elif operacao[3] == '-':
+                resultado = SubMatrizes(Ma, Mb)
+                m2 = resultado
+            elif operacao[3] == '.':
+                resultado = MultiMatrizes(Ma, Mb)
+                m2 = resultado
     elif operacao[0] == 'm2' and '*' in operacao:
         A = None
         B = None
