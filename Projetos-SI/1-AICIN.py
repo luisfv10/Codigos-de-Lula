ac766c3b2e5d4d8ecd129977ea7f7ea5911cff94
Luis Filipe Vasconcelos Pascoal <luisfilipevasconcelospascoal@gmail.com>
2022-10-12 09:21:07 -0300

    Create 1-AICIN.py

---
+++ Projetos-SI/1-AICIN.py
@@ -0,0 +1,9 @@
+# entrada
+comida = input()
+if(comida == 'Pamonha'):
+  print('Boa! Pamonha faz parte.')
+elif(comida == 'Canjica'):
+  print('Boa! Canjica faz parte.')
+elif(comida == 'Bolo de milho'):
+  print('Boa! Bolo de milho faz parte.')
+else: print('Opa! Parece que voce procura um tipo de comida que nao combina com essa epoca, tente novamente.')
